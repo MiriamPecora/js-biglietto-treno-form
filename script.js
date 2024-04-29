3a584bb5c7e4ad448f58490e3c5303b8332fde87
MiriamPecora <miriampecora97@gmail.com>
2024-04-29 19:59:58 +0200

    [UPD] Added ticket feature on d-none and linked inputs to ticket info.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let ageInput
 let kmInput
 const submitBtn = document.getElementById("submit");
+const cancelBtn = document.getElementById("cancel")
 const kmPrice = 0.21;
 let totalPrice
 let discount
+let userName
+
 
 submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
     
+    document.getElementById("ticket").style.display = "block"
+
     ageInput = document.getElementById("age").value;
     kmInput = document.getElementById("kms").value;
     console.log (ageInput, "questa è la tua età")
@@ -28,6 +33,22 @@ submitBtn.addEventListener('click', function (event) {
     
     totalPrice = Math.round(totalPrice * 100) / 100;
     
+    userName = document.getElementById("name_surname").value;
+    
+    console.log (userName, "questo è il tuo nome")
     console.log(totalPrice, "questo è il prezzo totale da pagare (con sconti applicati)");
+
+    document.getElementById("traveler_name").innerText=userName;
+    document.getElementById("travel_km").innerText=kmInput + "km";
+    document.getElementById("randomized_cart").innerText="9";
+    document.getElementById("applied_discount").innerText=discount;
+    document.getElementById("total_price").innerText=totalPrice;
+
 })
+
+cancelBtn.addEventListener("click", function () {
+    window.location.reload();
+})
+
+
 
