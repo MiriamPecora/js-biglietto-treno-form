29a3c50238e5b4e29d804839b22318594af0f12c
MiriamPecora <miriampecora97@gmail.com>
2024-04-29 17:51:06 +0200

    [COMPLETED MILESTONE 1] Fixed variables' name to be clearer when reading them

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let userAge
-let kmToTravel
+let ageInput
+let kmInput
 const submitBtn = document.getElementById("submit");
 const kmPrice = 0.21;
 let totalPrice
 let discount
 
 submitBtn.addEventListener('click', function () {
-    userAge = document.getElementById("age").value;
-    kmToTravel = document.getElementById("kms").value;
-    console.log (userAge, "questa è la tua età")
-    console.log(kmToTravel, "questi sono i km che vuoi percorrere")
-    totalPrice = kmToTravel * kmPrice;
+    ageInput = document.getElementById("age").value;
+    kmInput = document.getElementById("kms").value;
+    console.log (ageInput, "questa è la tua età")
+    console.log(kmInput, "questi sono i km che vuoi percorrere")
+    totalPrice = kmInput * kmPrice;
     console.log(totalPrice, "questo è il prezzo prima di essere scontato")
     
-    if (userAge < 18){
+    if (ageInput < 18){
         discount = totalPrice * 20 / 100;
         console.log(discount, "questo è lo sconto applicato ai minorenni");
         totalPrice = totalPrice - discount;
     }
-    else if (userAge >= 65){
+    else if (ageInput >= 65){
         discount = totalPrice * 40 / 100;
         console.log(discount, "questo è lo sconto applicato agli over 65");
         totalPrice = totalPrice - discount;
